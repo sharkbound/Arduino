a6d73039b52923c5e3cd290bdd726054ed6ee6e1
sharkbound <laptopblaster@gmail.com>
2015-07-22 17:00:02 -0500

    working on sodamachinescript

--- SodaMachineScript/SodaMachineScript.ino
+++ SodaMachineScript/SodaMachineScript.ino
@@ -1,12 +1,22 @@
+int increase = 10;
+int power = 10;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(9, OUTPUT);
-
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+for (int ii = 0; ii < 255; ii++)
+{
+ analogWrite(9, power);
+ power += increase;
+ delay(30);
+}
+
 
 }
--- arduino codes/lightvore_project/lightvore_project.ino
+++ arduino codes/lightvore_project/lightvore_project.ino
@@ -2,67 +2,61 @@ int count;
 int light1;
 int light2;
 int diferince;
-void lr(){
- light1= analogRead(A0);
- light2= analogRead(A1);
+void lr() {
+  light1 = analogRead(A0);
+  light2 = analogRead(A1);
 }
-void lp(){
+void lp() {
   Serial.println("photoresistor 1 is");
   Serial.println(light1);
   Serial.println("photoresistor 2 is");
   Serial.println(light2);
 }
-void difr(){
-diferince= light1 - light2;
+void difr() {
+  diferince = light1 - light2;
 }
-void difw(){
+void difw() {
   Serial.println(diferince);
 }
 
 
-void setup(){
-pinMode(A0, 0);
-pinMode(A1, 0);
-pinMode(3, 1);
-pinMode(5, 1);
-Serial.begin(9600);  
+void setup() {
+  pinMode(A0, 0);
+  pinMode(A1, 0);
+  pinMode(3, 1);
+  pinMode(5, 1);
+  Serial.begin(9600);
 }
-void loop(){
+void loop() {
   lr();
   difr();
   difw();
-  if(diferince >= -280){
+  
+  if (diferince >= -280) {
     digitalWrite(3, 1);
-   
-   
-   
   }
   else
   {
     digitalWrite(3, 0);
   }
-    
-    
-  if(diferince <= -293){
+  
+  if (diferince <= -293) {
     digitalWrite(5, 1);
-   
-    
   }
   else
   {
     digitalWrite(5, 0);
-  
-   }
-   
-  
-     
-  
-  
-  
-  
-  
-  
-  
-  
- 
+  }
+
+
+
+
+
+
+
+
+
+
+
+
 }
